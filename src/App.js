1467c244a6778080639ef87176f2cdaa1b355a35
ben <bjelinek42@gmail.com>
2022-11-12 20:28:27 -0600

    add input and button, change axios request to function

--- src/App.js
+++ src/App.js
@@ -8,21 +8,25 @@ function App() {
   const [pokemonInfo, setPokemonInfo] = useState({
     name: ""
   })
-  axios.get(`https://pokeapi.co/api/v2/pokemon/ditto`).then(response => {
-    let pokemonAttributes = response.data
-    setPokemonInfo({
-      name: pokemonAttributes.name
+
+  const search = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/ditto`).then(response => {
+      let pokemonAttributes = response.data
+      setPokemonInfo({
+        name: pokemonAttributes.name
+      })
+      console.log(pokemonInfo)
     })
-    console.log(pokemonInfo)
-  })
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter a pokemon: <input type="text" />
         </p>
+        <button onClick={(search)}>Get Info</button>
 
       </header>
     </div>
