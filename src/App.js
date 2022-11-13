3adb26be985ad03af416966e6f78a2d1e4a3c3be
ben <bjelinek42@gmail.com>
2022-11-12 20:12:02 -0600

    add pokemon logo and styling change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './images/pokemonLogo.png';
 import './App.css';
 import axios from 'axios'
 
@@ -16,14 +16,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
